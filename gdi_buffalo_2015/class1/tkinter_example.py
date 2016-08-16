dcb380e3fd24da8c94eebf0b234bd913f8c9e6a7
Izydorczak <anneizydorczak@gmail.com>
2016-08-16 10:08:59 -0400

    Update tkinter_example.py

--- gdi_buffalo_2015/class1/tkinter_example.py
+++ gdi_buffalo_2015/class1/tkinter_example.py
@@ -1,2 +1,22 @@
+import Tkinter
+import random
+
+my_gui = Tkinter.Tk()
+
+my_gui.title("Pypet")
+my_gui.geometry("500x300")
+
+
+def express_happy():
+        say_options =["I love you!", "Hip hip hurray!", "Purr, Purr"]
+        my_label = Tkinter.Label(my_gui, text = random.choice(say_options))
+        my_label.pack()
+
+feed_button = Tkinter.Button(my_gui, text = "Feed Me", command = express_happy)
+
+feed_button.pack()
+
+my_gui.mainloop()
+
 
     	
