6d29d6a7636e5b70b4208b4fd59bf91d569b6832
Izydorczak <anneizydorczak@gmail.com>
2016-11-09 16:09:18 -0500

    Create turtle_lesson.py

---
+++ gdi_buffalo_2015/class1/turtle_lesson.py
@@ -0,0 +1,39 @@
+
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+alex.forward(150)
+alex.left(90)
+alex.forward(75)
+wn.exitonclick()
+
+
+import turtle
+
+wn = turtle.Screen()
+alex = turtle.Turtle()
+alex.forward(100)
+alex.left(72)
+alex.forward(100)
+alex.left(72)
+alex.forward(100)
+alex.left(72)
+alex.forward(100)
+alex.left(72)
+alex.forward(100)
+alex.left(72)
+wn.exitonclick()
+
+
+for sideNum in [1,2,3,4,5]:
+  alex.forward(100)
+  alex.left(72)
+  
+  
+for sideNum in range(5):
+  alex.forward(100)
+  alex.left(72)
+  
+  
+alex.shape(
