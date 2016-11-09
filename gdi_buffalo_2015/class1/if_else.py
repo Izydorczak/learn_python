240e99ae57a62b2850bab778a1c667d853399ea8
Izydorczak <anneizydorczak@gmail.com>
2016-11-09 14:40:32 -0500

    Update if_else.py

--- gdi_buffalo_2015/class1/if_else.py
+++ gdi_buffalo_2015/class1/if_else.py
@@ -1,16 +1,16 @@
-hungry = raw_input(“Are you hungry? Type Y or N ”)
+hungry = input(“Are you hungry? Type Y or N ”)
 food = 0
 if hungry == “Y”:
-	print “Here is a treat for you.”
+	print (“Here is a treat for you.”)
 	food = food + 1
 else:
-	print “okay”
+	print (“okay”)
 
 pigs = “fly”
 if pigs == “fly”:
-	print “This prints.”
+	print (“This prints.”)
 else:
-	print “This does not print.”	
+	print (“This does not print.”)	
 
 import random
 x = random.randint(-10,10)
@@ -20,11 +20,11 @@ elif x ==0:
 	sign = “none”
 else:
 	sign = “positive”
-print sign, x
+print (sign, x)
 
 sign = “positive” if x > 0 else “negative”
 
-grade = raw_input(“Enter a whole number grade between 0 and 100 “)
+grade = input(“Enter a whole number grade between 0 and 100 “)
 if grade > 90:
 	print “A”
 elif grade < 90 and grade > 80:
@@ -32,39 +32,39 @@ elif grade < 90 and grade > 80:
 grade = raw_input(“Enter a whole number grade between 0 and 100 “)
 grade = int(grade)
 if grade > 90:
-	print “A”
+	print (“A”)
 elif grade > 80:
-	print “B”
+	print (“B”)
 elif grade > 70:
-	print "C"
+	print ("C")
 elif grade > 60
-	print "D"
+	print ("D")
 else:
-	print "F"
+	print ("F")
 
 x = 1
 y = 2
 if x == 1 and y > 1:
 	if y == 1:
-		print “tomato”
-	print “potato”
-print “cabbage”
+		print (“tomato”)
+	print (“potato”)
+print (“cabbage”)
 
 x = ‘sand’
 if ‘sand’ in ‘sandwiches’:
-	print x*3
+	print(x*3)
 	x += ‘wich’
 	if x.endswith(‘wich’):
 		x *=2
-		print x
+		print(x)
 
 x = [1,3,5,7,11]
 if x[0:2] != [1,3]:
-	print ‘nope’
+	print(‘nope’)
 	if x[4] == 11:
-		print ‘yup’
+		print(‘yup’)
 else:
-	print ‘yup yup’
+	print(‘yup yup’)
 	
 5 == 6
 5==5
