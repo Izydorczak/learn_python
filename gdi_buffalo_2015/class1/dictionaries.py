38d4cf4ae256d0dd1e8f818758fa35532844e3ba
Izydorczak <anneizydorczak@gmail.com>
2016-11-09 15:49:45 -0500

    Update dictionaries.py

--- gdi_buffalo_2015/class1/dictionaries.py
+++ gdi_buffalo_2015/class1/dictionaries.py
@@ -9,29 +9,29 @@ if __name__ == "__main__":
     # Make a new dictionary, using { instead of [.
     # Each pair of (key, value) items is connected with a colon.
     my_dict = { "Wendy": "Jansson", "Joe": "Jones", "Clara": "Rodriquez"}
-    print my_dict
-    print my_dict["Wendy"]
+    print(my_dict)
+    print(my_dict["Wendy"])
     
     # Add a new person to our dictionary.
     my_dict["Carey"] = "Forester"
-    print my_dict["Carey"]
-    print my_dict
+    print(my_dict["Carey"])
+    print(my_dict)
     
     # Can we store two different Claras?
     my_dict["Clara"] = "Wu"    # Remember up above we already said "Clara": "Rodriguez"!
-    print my_dict["Clara"]
+    print(my_dict["Clara"])
 
     #print my_dict["Anne"] # Comment this line out unless you want an error!
 
     # Store a PyPet in a dictionary.
     monty = {"age": 3, "name": "Monty", "type": "doberman"}
     for k in monty.keys():
-        print k, ":", monty[k]
+        print(k, ":", monty[k])
     
     # Store employees and update the dictionary when they come and go.
     emps = {1: "Rich Simons", 2: "Natalia Burges", 3: "Adrianne Velasquez", 4: "Ying Su"}
-    print emps
-    print "There are", len(emps), "total at the start."
+    print(emps)
+    print("There are", len(emps), "total at the start.")
     emps[5] = "Thomas Hah"
     emps.remove(3)
-    print "After some changes, the employee list is:", emps.values()
+    print("After some changes, the employee list is:", emps.values())
