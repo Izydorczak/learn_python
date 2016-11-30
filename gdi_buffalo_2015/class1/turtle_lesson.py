87827c1cc887ffe4b5d512cef3c22da6dde11407
Izydorczak <anneizydorczak@gmail.com>
2016-11-30 16:33:57 -0500

    Update turtle_lesson.py

--- gdi_buffalo_2015/class1/turtle_lesson.py
+++ gdi_buffalo_2015/class1/turtle_lesson.py
@@ -36,4 +36,41 @@ for sideNum in range(5):
   alex.left(72)
   
   
-alex.shape(
+alex.shape("turtle")
+alex.shape("square")
+alex.penup()
+alex.pendown()
+alex.circle(6)
+alex.pensize(4)
+
+
+
+for aColor in ["red", "blue", "yellow", "green", "purple"]:
+  alex.color(aColor)
+  alex.forward(100)
+  alex.left(72)
+  
+  
+  
+def drawSquare():
+  alex.penup()
+  alex.goto(50,50)
+  alex.pendown()
+  for side in range(4):
+      alex.forward(50)
+      alex.right(90)
+      
+      
+drawSquare()
+
+
+def drawSquare(x,y):
+  alex.penup()
+  alex.goto(x,y)
+  alex.pendown()
+  for side in range(4):
+      alex.forward(50)
+      alex.right(90)
+      
+drawSquare(50,50)
+drawSquare(100,200)
