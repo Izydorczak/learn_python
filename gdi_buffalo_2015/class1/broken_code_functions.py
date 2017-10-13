2e8206bb128236627fe6d6e5cc5c2f9231e8b109
Izydorczak <anneizydorczak@gmail.com>
2017-10-13 11:04:51 -0500

    Update broken_code_functions.py

--- gdi_buffalo_2015/class1/broken_code_functions.py
+++ gdi_buffalo_2015/class1/broken_code_functions.py
@@ -9,7 +9,7 @@ print (add_three(5) + add three(6) + add three(7))
 add_three(10,20,30)
 
 
-# add the digits 1-9
+# add the digits 1-9. This part has no errors.
 
-summ = add_three(add_three(1,2,3) + add_three(4,5,6) + add_three(7,8,9))
+summ = add_three(add_three(1,2,3),add_three(4,5,6),add_three(7,8,9))
 print (summ)
