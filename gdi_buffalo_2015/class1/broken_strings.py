16f174b09f0fa2dbbfa36dde44b8c5231f618e33
Izydorczak <anneizydorczak@gmail.com>
2017-10-17 11:17:09 -0500

    Update broken_strings.py

--- gdi_buffalo_2015/class1/broken_strings.py
+++ gdi_buffalo_2015/class1/broken_strings.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+
 
 #Fix this paragraph so that the line print(paragraph) at the end will print the paragraph
 #with all the groots capitalized and the first letter of each sentence capitalized. 
