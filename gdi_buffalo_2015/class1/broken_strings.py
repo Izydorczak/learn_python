c9bca9e783fe7eb0c0343dd93ebd6c02c6ac6a38
Izydorczak <anneizydorczak@gmail.com>
2017-10-17 11:44:10 -0500

    Update broken_strings.py

--- gdi_buffalo_2015/class1/broken_strings.py
+++ gdi_buffalo_2015/class1/broken_strings.py
@@ -4,3 +4,4 @@
 #with all the groots capitalized and the first letter of each sentence capitalized. 
 
 # print(paragraph) should give us the original paragraph, with all the groots capitalized
+paragraph = "i am groot. yeah we know who you are. i am groot. yeah you just said that. i wasn't retrieving the orb for ronan, i was betraying him. i am groot. well that's just as fascinating as the first eighty-nine times you told me that." 
