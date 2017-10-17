20617780f01655f7cb1d7d580e1de3f0e9ae6d9b
Izydorczak <anneizydorczak@gmail.com>
2017-10-17 11:43:23 -0500

    Update broken_strings.py

--- gdi_buffalo_2015/class1/broken_strings.py
+++ gdi_buffalo_2015/class1/broken_strings.py
@@ -4,27 +4,3 @@
 #with all the groots capitalized and the first letter of each sentence capitalized. 
 
 # print(paragraph) should give us the original paragraph, with all the groots capitalized
-paragraph = "i am groot. yeah we know who you are. i am groot. yeah you just said that. i wasn't retrieving the orb for ronan, i was betraying him. i am groot. well that's just as fascinating as the first eighty-nine times you told me that." 
-
-print("What follows is the paragraph with all the groots capitalized:\n ",paragraph.replace('groot', 'Groot'))
-
-print ("\n\nWhat follows should still be the paragraph with all the groots capitalized:\n ", paragraph)
-
-# split the paragraph into a list of sentences
-
-par_list = paragraph.split('.')
-print("\n\nThis is the paragraph split into a list:\n",par_list)
-
-#The empty sentence at the end causes a key error. You will need to remove it.
-
-# This loop will capitalize each sentence
-
-for sentence of par_list:
-  sentence = sentence.lstrip()  #remove space before sentence
-  sentence_list = list(sentence  #turn sentence into a list to make it mutable 
-  sentence_list[0]= sentence_list[0].upper  #capitalize the first letter
-  sentence = "".join(sentence_list)   #rejoin sentence
-  par_list_new.append(sentence)
-
-
-print("\n\nHere is the fixed paragraph:\n",paragraph)
